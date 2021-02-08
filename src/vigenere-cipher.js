139a230e7bfd5e79b1851f69714c0e05efc221f2
VanWilder199 <72692962+VanWilder199@users.noreply.github.com>
2021-02-08 19:51:05 +0300

    init: vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (alfa = true) {
+    this.alfa = alfa;
   }
-}
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('');
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+    let j = 0;
+    let crypt = '';
+  
+    for (let i = 0; i < message.length; i++){
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        
+        crypt += String.fromCharCode((message.charCodeAt(i) + key.charCodeAt(j % key.length) - 130) % 26 + 65);
+        j++;
+      }
+      else crypt += message[i];
+    }
+    if (this.alfa) return crypt;   
+    else return crypt.split('').reverse().join('');
+  }  
+
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('');
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+    let j = 0;
+    let dCrypt = '';
+    for (let i = 0; i < message.length; i++){
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        dCrypt += String.fromCharCode((message.charCodeAt(i) + 26 - key.charCodeAt(j % key.length)) % 26 + 65);
+        j++;
+      }
+      else dCrypt += message[i];
+    }
+    if (this.alfa) return dCrypt;   
+    else return dCrypt.split('').reverse().join('');
+  }
+  }
 
 module.exports = VigenereCipheringMachine;
